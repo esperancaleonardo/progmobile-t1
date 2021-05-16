38367eb899cb7d40253ccdc327342cb5b4efbee3
Leonardo <leonardo.oliveira.esperanca@gmail.com>
2021-05-15 22:30:14 -0400

    refactor chamadas dos fragmentos na main activity

--- projeto/Manager/app/src/main/java/com/example/manager/MainActivity.java
+++ projeto/Manager/app/src/main/java/com/example/manager/MainActivity.java
@@ -7,8 +7,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.navigation.NavigationView;
+
+import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,15 +34,11 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerToggle.setDrawerIndicatorEnabled(true);
         drawerToggle.syncState();
-
         drawerLayout.addDrawerListener(drawerToggle);
 
         //carregando o fragmento da página inicial para o contexto do framelayout
         CtxInit initFragment = new CtxInit();
-        transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.pagerContext, initFragment, "frag_init");
-        transaction.addToBackStack("pilha");
-        transaction.commit();
+        seleciona_fragmento(lastId, initFragment, "frag_init");
 
         navView = findViewById(R.id.nav_sidebar);
 
@@ -48,51 +47,27 @@ public class MainActivity extends AppCompatActivity {
             if (id != lastId){
                 if (id == R.id.menuCtxInicial){ //usuario seleciona contexto inicial para o viewpager
                     CtxInit initFragment1 = new CtxInit();
-                    transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.pagerContext, initFragment1, "frag_inicial");
-                    transaction.addToBackStack("pilha");
-                    transaction.commit();
-                    lastId = id;
+                    seleciona_fragmento(id, initFragment1, "frag_inicial");
                 }
                 else if (id == R.id.menuCtxCurso){ //usuario seleciona contexto cursos para o viewpager
                     CtxCursoFragment cursoFragment = new CtxCursoFragment();
-                    transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.pagerContext, cursoFragment, "frag_curso");
-                    transaction.addToBackStack("pilha");
-                    transaction.commit();
-                    lastId = id;
+                    seleciona_fragmento(id, cursoFragment, "frag_curso");
                 }
                 else if (id == R.id.menuCtxAluno){ //usuario seleciona contexto alunos para o viewpager
                     CtxAlunoFragment alunoFragment = new CtxAlunoFragment();
-                    transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.pagerContext, alunoFragment, "frag_aluno");
-                    transaction.addToBackStack("pilha");
-                    transaction.commit();
-                    lastId = id;
+                    seleciona_fragmento(id, alunoFragment, "frag_aluno");
                 }
                 else if (id == R.id.menuCtxLista){ //usuario seleciona contexto listar para o viewpager
                     CtxListagemFragment listagemFragment = new CtxListagemFragment();
-                    transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.pagerContext, listagemFragment, "frag_lista");
-                    transaction.addToBackStack("pilha");
-                    transaction.commit();
-                    lastId = id;
+                    seleciona_fragmento(id, listagemFragment, "frag_lista_cursos_cadastro");
                 }
                 else if (id == R.id.menuCtxRelAlunoCurso){ //usuario seleciona contexto listar qtde alunos por curso
                     CtxListagemQtdeFragment listagemQtdeFragment = new CtxListagemQtdeFragment();
-                    transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.pagerContext, listagemQtdeFragment, "frag_lista_alunos_curso");
-                    transaction.addToBackStack("pilha");
-                    transaction.commit();
-                    lastId = id;
+                    seleciona_fragmento(id, listagemQtdeFragment, "frag_lista_alunos_curso");
                 }
                 else if (id == R.id.menuCtxAbout){ //usuario seleciona contexto página sobre para o viewpager
                     CtxAbout aboutFragment = new CtxAbout();
-                    transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.pagerContext, aboutFragment, "frag_inicial");
-                    transaction.addToBackStack("pilha");
-                    transaction.commit();
-                    lastId = id;
+                    seleciona_fragmento(id, aboutFragment, "frag_about");
                 }
                 else if (id == R.id.menuCtxSair){ //usuario seleciona contexto sair do app para o viewpager
                     lastId = id;
@@ -104,5 +79,13 @@ public class MainActivity extends AppCompatActivity {
             drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         });
+    }
+
+    public void seleciona_fragmento(Integer id, Fragment fragment, String tag){
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.pagerContext, fragment, tag);
+        transaction.addToBackStack("pilha");
+        transaction.commit();
+        lastId = id;
     }
 }
