dae81a9ce3c813f7ced1d301227e2770c5535775
Leonardo <leonardo.oliveira.esperanca@gmail.com>
2021-05-15 22:57:58 -0400

    refactor nos contextos de aluno e curso

--- projeto/Manager/app/src/main/java/com/example/manager/CtxCursoFragment.java
+++ projeto/Manager/app/src/main/java/com/example/manager/CtxCursoFragment.java
@@ -7,27 +7,18 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class CtxCursoFragment extends Fragment {
-
-    ViewPager pagerTabLayoutCurso;
-    TabLayout tabLayoutCurso;
-    TabItem create, update, delete;
-    CtxCursoPagerAdapter adapter;
+    private ViewPager pagerTabLayoutCurso;
+    private TabLayout tabLayoutCurso;
+    private CtxCursoPagerAdapter adapter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_ctx_curso, container, false);
         pagerTabLayoutCurso = v.findViewById(R.id.pagerCurso);
         tabLayoutCurso = v.findViewById(R.id.tabCursoLay);
-        create = v.findViewById(R.id.tabCurso1);
-        update  = v.findViewById(R.id.tabCurso2);
-        delete = v.findViewById(R.id.tabCurso3);
-
         adapter = new CtxCursoPagerAdapter(getChildFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, tabLayoutCurso.getTabCount());
         adapter.notifyDataSetChanged();
         pagerTabLayoutCurso.setAdapter(adapter);
